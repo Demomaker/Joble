0d32ae7f5d2ff4e0a62f222ac92891f53ecf75cb
Mike Bédard <mike01bedard@gmail.com>
2022-01-15 14:32:08 -0500

    implemented some basic functionalities

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
-const menuTemplate = require('./projectJS/menuTemplate.js');
 const { Menu } = require('electron');
 const electron = require('electron');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const ipc = electron.ipcMain;
-const menu = Menu.buildFromTemplate(menuTemplate);
 
 function createWindow () {
     // Create the browser window.
     let mainWin = new BrowserWindow({
       resizable: false,
-      height: 720,
       width: 1280,
+      height: 720,
       webPreferences: {
         nodeIntegration: true
       }
